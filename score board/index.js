3aa9c7f836c0f2679838cba7130a4765e62e1593
vishnu326 <vishnush326@gmail.com>
2022-10-08 20:19:18 +0530

    changed

--- score board/index.js
+++ score board/index.js
@@ -1,33 +1,35 @@
-let count = 0
+let count1 = 0
+let count2 = 0
  let Display = document.getElementById("score-home")
  let DIsplay = document.getElementById("score-guest")
   function increase1(){
-      count += 1
-      Display.textContent = count
+      count1 += 1
+      Display.textContent = count1
   }
    function increase2(){
-       count +=2
-       Display.textContent = count
+       count1 +=2
+       Display.textContent = count1
    } 
     function increase3(){
-        count += 3
-        Display.textContent = count
+        count1 += 3
+        Display.textContent = count1
     }
     function increment1(){
-      count += 1
-      DIsplay.textContent = count
+      count2 += 1
+      DIsplay.textContent = count2
   }
    function increment2(){
-       count +=2
-       DIsplay.textContent = count
+       count2 +=2
+       DIsplay.textContent = count2
    } 
     function increment3(){
-        count += 3
-        DIsplay.textContent = count
+        count2 += 3
+        DIsplay.textContent = count2
     }
 
 function reset(){
     Display.innerText = 0
     DIsplay.innerText = 0
-    count = 0
+    count1 = 0
+    count2 = 0
 }
